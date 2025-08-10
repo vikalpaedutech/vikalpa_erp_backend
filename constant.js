cd7c33917d26174773bffd998c31af56ce81e8bc
Shubham <mb.shbuhamshah@gmail.com>
2025-08-11 03:27:39 +0530

    constant fixing

--- constant.js
+++ constant.js
@@ -1,9 +1,9 @@
-// export const DB_NAME = "vikalpa";
+export const DB_NAME = "vikalpa";
 
 
 //Below is for testing purpose. Deactivate it whnen pushoing code into production
 
-export const DB_NAME = "vikalpa_testing";
+// export const DB_NAME = "vikalpa_testing";
 
 
 
